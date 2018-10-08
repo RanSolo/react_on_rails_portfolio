562cdffb91d14bd70e233c835522e51f6afc2b53
Randy Russell <ransolo@me.com>
2018-10-08 14:55:33 -0500

    add-react-strap-and-AppNavbar-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,82 +1,15 @@
 import React, { Component } from 'react'
-import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
+import AppNavbar from './components/AppNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {}
-    this.getExamples = this.getExamples.bind(this)
-    this.getExample = this.getExample.bind(this)
-  }
-
-  componentDidMount () {
-    this.getExamples()
-  }
-
-  fetch (endpoint) {
-    return window.fetch(endpoint)
-      .then(response => response.json())
-      .catch(error => console.log(error))
-  }
-
-  getExamples () {
-    this.fetch('/api/examples')
-      .then(examples => {
-        if (examples.length) {
-          this.setState({examples: examples})
-          this.getExample(examples[0].id)
-        } else {
-          this.setState({examples: []})
-        }
-      })
-  }
-
-  getExample (id) {
-    this.fetch(`/api/examples/${id}`)
-      .then(example => this.setState({example: example}))
-  }
-
   render () {
-    let {examples, example} = this.state
-    return examples
-      ? <Container text>
-        <Header as='h2' icon textAlign='center' color='teal'>
-          <Icon name='unordered list' circular />
-          <Header.Content>
-            Code Examples
-          </Header.Content>
-        </Header>
-        <Divider hidden section />
-      {examples && examples.length
-          ? <Button.Group color='teal' fluid widths={examples.length}>
-            {Object.keys(examples).map((key) => {
-              return <Button active={example && example.id === examples[key].id} fluid key={key} onClick={() => this.getExample(examples[key].id)}>
-                {examples[key].title}
-              </Button>
-            })}
-          </Button.Group>
-          : <Container textAlign='center'>No examples found.</Container>
-        }
-        <Divider section />
-      {example &&
-          <Container>
-            <Header as='h2'>{example.title}</Header>
-          {example.description && <p>{example.description}</p>}
-            {example.code_blocks &&
-              <Segment.Group>
-                {example.code_blocks.map((code_block, i) => <Segment key={i}>{code_block.description}</Segment>)}
-              </Segment.Group>
-            }
-            {example.steps && <p>{example.steps}</p>}
-            {example.source && <Button basic size='tiny' color='teal' href={example.source}>Source</Button>}
-          </Container>
-        }
-      </Container>
-      : <Container text>
-        <Dimmer active inverted>
-          <Loader content='Loading' />
-        </Dimmer>
-      </Container>
+    return (
+      <div className="App">
+        <AppNavbar />
+      </div>
+    );
   }
 }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+AdminUser.create!(email: 'admin@example.com', password: 'password',
+                  password_confirmation: 'password')
 
 # Examples
 api_creation = Example.create(
   title: 'RESTful API creation.',
-  description: 'This site is a Rails API backend with a React front end.  It both creates and consumes an API'
+  description: 'This site is a Rails API backend with a React front end.  It
+                both creates and consumes an API'
 )
 
 cool_web_stack = Example.create(
@@ -22,7 +19,12 @@ cool_web_stack = Example.create(
 # Associated URLs
 api_creation.urls.create(
   actual: 'https://blog.heroku.com/a-rock-solid-modern-web-stack',
-  description: 'Rails is an incredible framework, but modern web development has moved to the front-end, meaning sometimes you don’t need all the bulk of the asset pipeline and the templating system. In Rails 5 you can now create an API-only Rails app, meaning you can build your front-end however you like—using Create React App, for example.',
+  description: 'Rails is an incredible framework, but modern web development has
+                moved to the front-end, meaning sometimes you don’t need all the
+                bulk of the asset pipeline and the templating system. In Rails 5
+                you can now create an API-only Rails app, meaning you can build
+                your front-end however you like—using Create React App, for
+                example.',
   url_type: 1)
 api_creation.urls.create(
   actual: 'www.ransolo_dev_portfolio.com',
@@ -30,7 +32,8 @@ api_creation.urls.create(
   url_type: 0)
 api_creation.urls.create(
   actual: 'https://www.dropbox.com/s/j8sso0ni13b0euc/Screenshot%202018-10-04%2017.16.40.png?dl=0',
-  description: 'Screenshot of JSON api/examples only one example is created  with one  related URL.',
+  description: 'Screenshot of JSON api/examples only one example is created
+                with one related URL.',
   url_type: 2)
 
 # Associated code_blocks
@@ -38,27 +41,27 @@ code_block = api_creation.code_blocks.create(
   location: 'Examples Controller',
   description: 'Shows the GET verbs for example index and show data queries.',
   block: '
-  # Examples controller inherits from the ApiControllerclass
-  class ExamplesController < ApiController
-    # GET /examples
-    def index
-      @examples = Example.select("id, title, description").all
-      render json: @examples.to_json(
-        :include => { :urls => { :only => [:id, :url_type, :description] },
-        :code_blocks => { :only => [:id, :location] } }
-      )
-    end
+    # Examples controller inherits from the ApiControllerclass
+    class ExamplesController < ApiController
+      # GET /examples
+      def index
+        @examples = Example.select("id, title, description").all
+        render json: @examples.to_json(
+          :include => { :urls => { :only => [:id, :url_type, :description] },
+          :code_blocks => { :only => [:id, :location] } }
+        )
+      end
 
-    # GET /examples/1
-    def show
-      @example = Example.find(params[:id])
-      render json: @example.to_json(
-        :include => { :urls => { :only => [:id, :description,
-                                           :actual, :url_type] },
-        :code_blocks => { :only => [:id, :location, :block, :description] }}
-      )
-    end
-  end')
+      # GET /examples/1
+      def show
+        @example = Example.find(params[:id])
+        render json: @example.to_json(
+          :include => { :urls => { :only => [:id, :description,
+                                             :actual, :url_type] },
+          :code_blocks => { :only => [:id, :location, :block, :description] }}
+        )
+      end
+    end')
 
 code_block.urls.create(
   actual: 'www.screenshot.com', url_type: 2,
