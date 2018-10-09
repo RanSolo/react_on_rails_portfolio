7f4a2781a9f1c7fb21600d22e9c756288cb1a3fc
Randy Russell <ransolo@me.com>
2018-10-09 16:06:25 -0500

    add-axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ExampleList from './components/ExampleList';
 import ExampleModal from './components/ExampleModal';
