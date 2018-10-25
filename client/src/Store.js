c5ab3ff656a6fc85754f9407c0d4a7b592908e18
Randy Russell <ransolo@me.com>
2018-10-25 10:54:57 -0500

    update-Store.js-for-redux-devtools-to-work-in-safar-and-firefox

--- client/src/Store.js
+++ client/src/Store.js
@@ -3,12 +3,13 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
